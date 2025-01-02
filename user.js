046ad9f1d0969379d609b052ab19eb0c9bed032d
UmBarril <74427600+UmBarril@users.noreply.github.com>
2025-01-02 20:08:57 -0300

    user.js and userChrome.css for firefox

---
+++ user.js
@@ -0,0 +1,23 @@
+
+/* disable about:config warning */
+user_pref("browser.aboutConfig.showWarning", false);
+
+/* experimental tab groups on firefox */
+user_pref("browser.tabs.groups.enabled", true);
+
+/* disable middle mouse paste */
+user_pref("middlemouse.paste", false);
+
+/* disable pocket */
+/* https://support.mozilla.org/en-US/kb/save-web-pages-later-pocket-firefox */
+/* https://github.com/pyllyukko/user.js/issues/143 */
+user_pref("browser.pocket.enabled",				false);
+user_pref("extensions.pocket.enabled",			false);
+
+/* vertical tabs */
+/* don't work very well on the current state (firefox 133) */
+// user_pref("sidebar.revamp",				false);
+// user_pref("sidebar.verticalTabs",		false);
+
+/* enable userChrome.css */
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
