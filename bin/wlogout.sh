a019dcabaf0fb81d753cc471ddce0f8a52d9431b
UmBarril <74427600+UmBarril@users.noreply.github.com>
2025-09-21 14:56:43 -0300

    fixing some stuff

--- bin/wlogout.sh
+++ bin/wlogout.sh
@@ -15,14 +15,18 @@ if [ $mon_width -eq 1366 ] && [ $mon_height -eq 768 ]; then
     # using 1366x768 values as base for scaling
     A_1366=300
     B_1366=300
-    wlogout -C $HOME/.config/wlogout/nova.css -l $HOME/.config/wlogout/layout --protocol layer-shell -b 5 -T $(awk "BEGIN {printf \"%.0f\", $A_1366 * $hypr_scale}") -B $(awk "BEGIN {printf \"%.0f\", $B_1366 * $hypr_scale}") &
+    A=$(awk "BEGIN {printf \"%.0f\", $A_1366 * $hypr_scale}")
+    B=$(awk "BEGIN {printf \"%.0f\", $B_1366 * $hypr_scale}")
+    wlogout -C $XDG_CONFIG_HOME/wlogout/nova.css -l $XDG_CONFIG_HOME/wlogout/layout --protocol layer-shell -b 5 -T $A -B $B &
 
     exit 0
 fi
 
 # using 1080p values as base for scaling
-A_1080=400
-B_1080=400
+A_1080=550
+B_1080=550
 
-wlogout -C $HOME/.config/wlogout/nova.css -l $HOME/.config/wlogout/layout --protocol layer-shell -b 5 -T $(awk "BEGIN {printf \"%.0f\", $A_1080 * 1080 * $hypr_scale / $mon_height}") -B $(awk "BEGIN {printf \"%.0f\", $B_1080 * 1080 * $hypr_scale / $resolution}") &
+A=$(awk "BEGIN {printf \"%.0f\", $A_1080 * 1080 * $hypr_scale / $mon_height}")
+B=$(awk "BEGIN {printf \"%.0f\", $B_1080 * 1080 * $hypr_scale / $mon_width}")
+wlogout -C $XDG_CONFIG_HOME/wlogout/nova.css -l $XDG_CONFIG_HOME/wlogout/layout --protocol layer-shell -b 5 -T $A -B $B &
 
